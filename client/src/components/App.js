710f306c0c8ee3bfa6e0c00380283db054b27bd8
gabinochacon8 <gabino.chacon8@gmail.com>
2021-09-16 00:28:09 -0700

    Actual working version before I possibly messed it up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,69 +1,68 @@
-import React from 'react';
-import NavBar from './NavBar';
-import MainPage from './MainPage';
-import AboutMe from './AboutMe';
-import MyWork from './MyWork';
-import MoabVid from './video/MoabCompile.mp4';
-import profilePic from '../assets/proPic.png';
-import { CSSTransition } from 'react-transition-group';
-import { HashRouter, BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
+import NavBar from "./NavBar";
+import MainPage from "./MainPage";
+import AboutMe from "./AboutMe";
+import MyWork from "./MyWork";
+import MoabVid from "./video/MoabCompile.mp4";
+import profilePic from "../assets/proPic.png";
+import { CSSTransition } from "react-transition-group";
+import {
+  HashRouter,
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
 
 function App(props) {
-
   const routes = [
-    { path: '/', name: 'Home', Component: Home },
-    { path: '/mywork', name: 'My Work', Component: MyWork },
-    { path: '/intro', name: 'Intro', Component: MainPage },
-    { path: '/about', name: 'About', Component: AboutMe },
-  ]
+    { path: "/", name: "Home", Component: Home },
+    { path: "/mywork", name: "My Work", Component: MyWork },
+    { path: "/intro", name: "Intro", Component: MainPage },
+    { path: "/about", name: "About", Component: AboutMe },
+  ];
 
   return (
     <Router>
-      <div style={{alignItems:'center', textAlign:'center'}}>
+      <div style={{ alignItems: "center", textAlign: "center" }}>
         <NavBar />
-        <div className="container">
-          {routes.map(({ path, Component }) => (
-            <Route key={path} exact path={path}>
-              {({ match }) => (
-                <CSSTransition
-                  in={match != null}
-                  timeout={300}
-                  classNames="page"
-                  unmountOnExit
-                >
-                  <div className="page">
-                    <Component />
-                  </div>
-                </CSSTransition>
-              )}
-            </Route>
-          ))}
-        </div>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={300}
+                classNames="page"
+                unmountOnExit
+              >
+                <div className="page">
+                  <Component />
+                </div>
+              </CSSTransition>
+            )}
+          </Route>
+        ))}
       </div>
     </Router>
   );
 }
 
 const Home = () => {
-    return (
-        <div className="videoContainer" >
-            <video autoPlay loop muted className="video" >
-                <source src={MoabVid} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-
-            <div className="content">
-                <div className="subContent" >
-                    <h1 className="myNameHeadline">GABE CHACON</h1>
-                    <p>Software Engineer and Marine Veteran</p>
-                    <img
-                        src={profilePic}
-                        alt="profile"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="videoContainer">
+      <video autoPlay playsinline loop muted autobuffer className="video">
+        <source src={MoabVid} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <div className="subContent">
+          <h1 className="myNameHeadline">GABE CHACON</h1>
+          <p>Software Engineer and Marine Veteran</p>
+          <img src={profilePic} alt="profile" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default App;
